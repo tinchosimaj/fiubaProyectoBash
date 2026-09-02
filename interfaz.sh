5c066d65f0c09ca3226a4607cb9b804b463aa1e9
Gabriel_Alexander <gsaavedrag@fi.uba.ar>
2026-09-02 10:19:19 -0300

    feat Agregando la logica de la opcion 4

--- interfaz.sh
+++ interfaz.sh
@@ -52,29 +52,40 @@ until [ "$numero" = '7' ]; do
   read numero
 
   case "$numero" in
-  1)
-    mkdir -p "$HOME/EPNro1/entrada" "$HOME/EPNro1/salida" "$HOME/EPNro1/procesado"
-    cp "$SCRIPT_DIR/consolidar.sh" "$HOME/EPNro1/consolidar.sh"
-    chmod +x "$HOME/EPNro1/consolidar.sh"
-    echo "Entorno creado en $HOME/EPNro1"
-    ;;
-	2)  echo "Elegiste la opción 2";;
-  3) 
-    if [ -z "$FILENAME" ]; then
-      echo "La variable de entorno FILENAME no está definida."
-    else
-      ARCHIVO="$HOME/EPNro1/salida/$FILENAME.txt"
+    1)
+      mkdir -p "$HOME/EPNro1/entrada" "$HOME/EPNro1/salida" "$HOME/EPNro1/procesado"
+      cp "$SCRIPT_DIR/consolidar.sh" "$HOME/EPNro1/consolidar.sh"
+      chmod +x "$HOME/EPNro1/consolidar.sh"
+      echo "Entorno creado en $HOME/EPNro1"
+      ;;
+	  2)  echo "Elegiste la opción 2";;
+    3) 
+      if [ -z "$FILENAME" ]; then
+        echo "La variable de entorno FILENAME no está definida."
+      else
+        ARCHIVO="$HOME/EPNro1/salida/$FILENAME.txt"
+          if [ -f "$ARCHIVO" ]; then
+            sort -n "$ARCHIVO"
+          else
+            echo "Error: El archivo de salida '$ARCHIVO' no existe."
+          fi
+        fi
+      ;;
+	  4)
+      if [ -z "$FILENAME" ]; then
+        echo "La variable de entorno FILENAME no está definida."
+      else
+        ARCHIVO="$HOME/EPNro1/salida/$FILENAME.txt"
         if [ -f "$ARCHIVO" ]; then
-          sort -n "$ARCHIVO"
+          awk '{print $NF"\t"$0}' "$ARCHIVO" | sort -n -k1,1 -r | cut -f2- | head -n 10
         else
           echo "Error: El archivo de salida '$ARCHIVO' no existe."
         fi
       fi
-    ;;
-	4) echo "Elegiste la opción 4";;
-	5) echo "Elegiste la opción 5";;
-	6) echo "Elegiste la opción 6";;
-	7) echo "Saliendo del menú" ;;
+      ;;
+	  5) echo "Elegiste la opción 5";;
+	  6) echo "Elegiste la opción 6";;
+	  7) echo "Saliendo del menú" ;;
 
     *) 
     echo "Opción inválida. Ingrese un número del 1 al 7.";;
